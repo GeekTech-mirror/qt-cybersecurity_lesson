71e0b0997e5fc4418f6b508cea244f99b0112784
Troy Hoover <troytjh98@gmail.com>
2022-03-03 14:38:08 -0600

    03/03/22

--- QT_Application/build-QT_Application-qemuarm64-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/usr/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/Drive/.git_repo/qt6_toolchain/qemuarm64/sysroots/x86_64-pokysdk-linux/usr/bin/qmake" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/Drive/.git_repo/qt6_toolchain/qemuarm64/sysroots/cortexa57-poky-linux/usr" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/Drive/.git_repo/qt6_toolchain/qemuarm64/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/Drive/.git_repo/qt6_toolchain/qemuarm64/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-g++" CACHE "STRING" "" FORCE)
